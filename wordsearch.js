3163bef733593fdbc18f0eedbe0603be4d158fb9
Peteami <peteami@hotmail.com>
2020-09-06 20:35:51 +0000

    second function commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let line of horizontalJoin) {
+    if (line.includes(word))
+      return true;
+  }
 
-module.exports = wordSearch
+
+  let finalArray = [];
+  const verticalJoin = letters.map((row, index) => {
+    row.map((element, oIndex) => {
+      if (index === 0) {
+        const temp = [];
+        temp.push(letters[index][oIndex]);
+        finalArray.push(temp);
+      } else {
+        finalArray[oIndex].push(letters[index][oIndex]);
+      }
+    });
+  });
+
+
+  for (let l of finalArray.map(element => element.join(''))) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const horizontalRevJoin = letters.map(ls => ls.reverse().join(''));
+  for (let l of horizontalRevJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+
+module.exports = wordSearch;
